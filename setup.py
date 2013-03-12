4974cc1c3f4c6b07d39192d682c042e0c07967f1
Martin Mrazik <martin.mrazik@canonical.com>
2013-03-12 09:31:51 +0100

    added split mode (.bzr-builddeb/default.conf), removed cupstream2distro-config.install and moved the data install into setup.py, few minor improvements in debian/control

--- setup.py
+++ setup.py
@@ -2,6 +2,10 @@
 
 from distutils.core import setup
 from setuptools import find_packages
+import os
+
+def get_files(directory):
+    return [os.path.join(directory, f) for f in os.listdir(directory)]
 
 setup(
     name='cupstream2distro-config',
@@ -13,5 +17,17 @@ setup(
         'ci/cu2d-trigger',
         'ci/cu2d-update-ci',
         'daily-release/cu2d-update-stack'
+    ],
+    data_files=[
+        ('/usr/share/cupstream2distro-config/ci/config',
+         get_files('ci/config')),
+        ('/usr/share/cupstream2distro-config/ci/jenkins-templates',
+         get_files('ci/jenkins-templates')),
+        ('/usr/share/cupstream2distro-config/daily-release/config',
+         get_files('daily-release/config')),
+        ('/usr/share/cupstream2distro-config/daily-release/jenkins-templates',
+         get_files('daily-release/jenkins-templates/')),
+        ('/etc/cupstream2distro-config/stacks',
+         get_files('stacks'))
     ],
 )
