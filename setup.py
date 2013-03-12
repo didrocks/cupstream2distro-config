86a9fa42eba31a54bf1025d0a021a6875554b92d
Martin Mrazik <martin.mrazik@canonical.com>
2013-03-12 08:26:15 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='cupstream2distro-config',
+    version='1.0',
+    url='https://launchpad.net/cupstream2distro-config',
+    packages=find_packages(),
+    test_suite='tests',
+    scripts=[
+        'ci/cu2d-trigger',
+        'cu2d-update-ci',
+        'daily-release/cu2d-update-stack'
+    ],
+)
